3320a88d34e8a818f6d98a17ab6ae753aa541e0a
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-07 18:38:43 +0100

    Add calculation logic for Higher rate taxpayers

--- main.py
+++ main.py
@@ -11,4 +11,11 @@ def calculate_income_taxes(salary: float) -> dict[str, float]:
 
 def calculate_income_tax_owed(salary: float) -> float:
     tax_free_allowance = 12_570
-    return (salary - tax_free_allowance) * 0.2
+    basic_rate_threshold = 50_270
+
+    if salary <= basic_rate_threshold:
+        return (salary - tax_free_allowance) * 0.20
+
+    return (basic_rate_threshold - tax_free_allowance) * 0.20 + (
+        salary - basic_rate_threshold
+    ) * 0.40
