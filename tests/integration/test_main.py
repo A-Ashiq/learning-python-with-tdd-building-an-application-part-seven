6576ccb668b50873801b3fde6d0deac6a569d140
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-07 18:51:07 +0100

    Add test to ensure calculated tax owed is 0 when salary is under personal allowance

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -50,3 +50,24 @@ class TestCalculateIncomeTaxOwed:
 
         # Then
         assert calculated_tax_owed == expected_tax_owed
+
+    @pytest.mark.parametrize(
+        "salary",
+        (0, 10, 100, 200, 1_000, 5_000, 12_568, 12_569),
+    )
+    def test_calculates_no_tax_when_salary_is_lower_than_personal_allowance(
+        self, salary: int
+    ):
+        """
+        Given a salary which lower than the personal allowance
+        When `calculate_income_tax_owed()` is called
+        Then the correct calculated tax owed of 0 is returned
+        """
+        # Given
+        input_salary = salary
+
+        # When
+        calculated_tax_owed: int = calculate_income_tax_owed(salary=input_salary)
+
+        # Then
+        assert calculated_tax_owed == 0
