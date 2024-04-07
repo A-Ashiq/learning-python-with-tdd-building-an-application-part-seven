2784e4809fb75a92fd9dd041f54b0b2039eda341
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-07 18:35:05 +0100

    Add logic to `GET /income-taxes` endpoint to calculate taxes for Basic rate taxpayers

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ app = FastAPI()
 
 
 @app.get(path="/income-taxes")
-def calculate_income_taxes(salary: float) -> dict[str, int]:
-    return {"tax owed": 4_086}
+def calculate_income_taxes(salary: float) -> dict[str, float]:
+    tax_free_allowance = 12_570
+    tax_owed = (salary - tax_free_allowance) * 0.2
+
+    return {"tax owed": tax_owed}
