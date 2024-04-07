f5acbe5aaca0114a70c198eb8b9cdde5cef1c3d9
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-07 18:37:46 +0100

    Add parametrized test for Higher rate taxpayers via `calculate_income_tax_owed()` function

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -2,7 +2,9 @@ from http import HTTPStatus
 
 from starlette.testclient import TestClient
 
-from main import app
+from main import app, calculate_income_tax_owed
+
+import pytest
 
 
 class TestIncomeTaxesEndpoint:
@@ -22,3 +24,32 @@ class TestIncomeTaxesEndpoint:
         # Then
         assert response.status_code == HTTPStatus.OK
         assert response.json() == {"tax owed": 4_086}
+
+
+...
+
+
+class TestCalculateIncomeTaxOwed:
+    @pytest.mark.parametrize(
+        "salary, expected_tax_owed",
+        (
+            [60_000, 11_432],
+            [65_000, 13_432],
+        ),
+    )
+    def test_calculates_for_higher_rate_banding(
+        self, salary: int, expected_tax_owed: float
+    ):
+        """
+        Given a salary which is in the higher rate banding
+        When `calculate_income_tax_owed()` is called
+        Then the correct calculated tax owed is returned
+        """
+        # Given
+        input_salary = salary
+
+        # When
+        calculated_tax_owed: float = calculate_income_tax_owed(salary=input_salary)
+
+        # Then
+        assert calculated_tax_owed == expected_tax_owed
