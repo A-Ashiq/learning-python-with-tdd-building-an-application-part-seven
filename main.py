85de06f02a8a7bbe93aca47d6251f67936e1928b
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-07 18:36:04 +0100

    Split calculation details of taxes out of `GET /income-taxes` endpoint into dedicated function

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ app = FastAPI()
 
 @app.get(path="/income-taxes")
 def calculate_income_taxes(salary: float) -> dict[str, float]:
-    tax_free_allowance = 12_570
-    tax_owed = (salary - tax_free_allowance) * 0.2
-
+    tax_owed: float = calculate_income_tax_owed(salary=salary)
     return {"tax owed": tax_owed}
+
+
+def calculate_income_tax_owed(salary: float) -> float:
+    tax_free_allowance = 12_570
+    return (salary - tax_free_allowance) * 0.2
