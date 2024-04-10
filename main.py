936e147ebadf77ddf29a6334f01199ea72098a5b
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-10 19:25:51 +0100

    Remove original and now redundant `main.py`

--- main.py
+++
@@ -1,8 +0,0 @@
-from domain.taxes import calculate_income_tax_owed
-from interfaces.api.main import app
-
-
-@app.get(path="/income-taxes")
-def calculate_income_taxes(salary: float) -> dict[str, float]:
-    tax_owed: float = calculate_income_tax_owed(salary=salary)
-    return {"tax owed": tax_owed}
