a068d44e859e550d6293bfb43facc9607d403c90
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-17 19:33:42 +0100

    Implement calculation logic for additional rate tax payers

--- domain/taxes.py
+++ domain/taxes.py
@@ -1,6 +1,7 @@
 def calculate_income_tax_owed(salary: float) -> float:
     tax_free_allowance = 12_570
     basic_rate_threshold = 50_270
+    higher_rate_threshold = 125140
 
     if salary <= tax_free_allowance:
         return 0
@@ -8,6 +9,13 @@ def calculate_income_tax_owed(salary: float) -> float:
     if salary <= basic_rate_threshold:
         return (salary - tax_free_allowance) * 0.20
 
-    return (basic_rate_threshold - tax_free_allowance) * 0.20 + (
+    if salary <= higher_rate_threshold:
+        return (basic_rate_threshold - tax_free_allowance) * 0.20 + (
             salary - basic_rate_threshold
-    ) * 0.40
+        ) * 0.40
+
+    return (
+        (basic_rate_threshold - tax_free_allowance) * 0.20
+        + (higher_rate_threshold - basic_rate_threshold) * 0.40
+        + (salary - higher_rate_threshold) * 0.45
+    )
