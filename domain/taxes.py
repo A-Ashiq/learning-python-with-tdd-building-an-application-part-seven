f572c81626be4b52b736d29cdefb5751602b5b81
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-27 15:34:59 +0100

    Add type hints

--- domain/taxes.py
+++ domain/taxes.py
@@ -16,17 +16,17 @@ def calculate_income_tax_owed(salary: float) -> float:
     return _calculate_additional_rate_tax_owed(salary=salary)
 
 
-def _calculate_basic_rate_tax_owed(salary) -> float:
+def _calculate_basic_rate_tax_owed(salary: float) -> float:
     return (salary - PERSONAL_ALLOWANCE_THRESHOLD) * 0.20
 
 
-def _calculate_higher_rate_tax_owed(salary) -> float:
+def _calculate_higher_rate_tax_owed(salary: float) -> float:
     return (HIGHER_RATE_THRESHOLD - PERSONAL_ALLOWANCE_THRESHOLD) * 0.20 + (
         salary - HIGHER_RATE_THRESHOLD
     ) * 0.40
 
 
-def _calculate_additional_rate_tax_owed(salary) -> float:
+def _calculate_additional_rate_tax_owed(salary: float) -> float:
     return (
         (HIGHER_RATE_THRESHOLD - PERSONAL_ALLOWANCE_THRESHOLD) * 0.20
         + (ADDITIONAL_RATE_THRESHOLD - HIGHER_RATE_THRESHOLD) * 0.40
